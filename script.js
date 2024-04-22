e97ca3b8d6b5c0fdd5c865cb1740bde1ac79e01b
igorsokolenko797 <58339058+igorsokolenko797@users.noreply.github.com>
2024-04-22 23:30:00 +0400

    Add Modal Window

---
+++ script.js
@@ -0,0 +1,47 @@
+const modelController = ({modal, btnOpen, btnClose, time = 300}) => {
+    const buttonElem = document.querySelector(btnOpen);
+    const modelElem = document.querySelector(modal);
+
+    modelElem.style.cssText = `
+    display: flex;
+    visibility: hidden;
+    opacity: 0;
+    z-index: 3;
+    transition: opacity ${time}ms ease-in-out;
+    `;
+
+    const closeModel = event => {
+        const target = event.target;
+
+        if (
+            target === modelElem ||
+            (btnClose && target.closest(btnClose)) ||
+            event.code === 'Escape'
+        ) {
+
+            modelElem.style.opacity = 0;
+
+            setTimeout(() => {
+                modelElem.style.visibility = 'hidden';
+            }, time);
+
+            window.removeEventListener('keydown', closeModel);
+        }
+    }
+
+    const openModel = () => {
+        modelElem.style.visibility = 'visible';
+        modelElem.style.opacity = '1';
+        window.addEventListener('keydown', closeModel)
+    };
+
+    buttonElem.addEventListener('click', openModel);
+    modelElem.addEventListener('click', closeModel);
+};
+
+modelController({
+    modal: '.model',
+    btnOpen: '.callback',
+    btnClose: '.btn-close'
+});
+
