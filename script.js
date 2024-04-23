15c18270113d458379ddde82fa8ec388cf01c378
Tigril6336 <ilnaz051107@gmail.com>
2024-04-23 20:12:47 +0300

    откат

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const modelController = ({modal, btnOpen, btnClose, time = 300}) => {
-    const buttonElem = document.querySelector(btnOpen);
-    const modelElem = document.querySelector(modal);
+const modelController = ({ modal, btnOpen, btnClose, time = 300 }) => {
+  const buttonElem = document.querySelector(btnOpen);
+  const modelElem = document.querySelector(modal);
 
-    modelElem.style.cssText = `
+  modelElem.style.cssText = `
     display: flex;
     visibility: hidden;
     opacity: 0;
@@ -10,38 +10,36 @@ const modelController = ({modal, btnOpen, btnClose, time = 300}) => {
     transition: opacity ${time}ms ease-in-out;
     `;
 
-    const closeModel = event => {
-        const target = event.target;
+  const closeModel = (event) => {
+    const target = event.target;
 
-        if (
-            target === modelElem ||
-            (btnClose && target.closest(btnClose)) ||
-            event.code === 'Escape'
-        ) {
+    if (
+      target === modelElem ||
+      (btnClose && target.closest(btnClose)) ||
+      event.code === "Escape"
+    ) {
+      modelElem.style.opacity = 0;
 
-            modelElem.style.opacity = 0;
+      setTimeout(() => {
+        modelElem.style.visibility = "hidden";
+      }, time);
 
-            setTimeout(() => {
-                modelElem.style.visibility = 'hidden';
-            }, time);
-
-            window.removeEventListener('keydown', closeModel);
-        }
+      window.removeEventListener("keydown", closeModel);
     }
+  };
 
-    const openModel = () => {
-        modelElem.style.visibility = 'visible';
-        modelElem.style.opacity = '1';
-        window.addEventListener('keydown', closeModel)
-    };
+  const openModel = () => {
+    modelElem.style.visibility = "visible";
+    modelElem.style.opacity = "1";
+    window.addEventListener("keydown", closeModel);
+  };
 
-    buttonElem.addEventListener('click', openModel);
-    modelElem.addEventListener('click', closeModel);
+  buttonElem.addEventListener("click", openModel);
+  modelElem.addEventListener("click", closeModel);
 };
 
 modelController({
-    modal: '.model',
-    btnOpen: '.callback',
-    btnClose: '.btn-close'
+  modal: ".model",
+  btnOpen: ".callback",
+  btnClose: ".btn-close",
 });
-
